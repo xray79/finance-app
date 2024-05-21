bd8f131d6cb54d33c7591f0f6f93bcb7543e11cf
Ibnul Huq <ibnul.huq@gmail.com>
2024-05-21 09:33:27 +0100

    create api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [
     react()
   ],
-  server: {
-      host: '0.0.0.0',
-      port: 3000
-    }
+  // server: {
+  //     host: '0.0.0.0',
+  //     port: 3000
+  //   }
 })
